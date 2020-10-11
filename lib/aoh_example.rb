fb820d3a9f98475dc26799d9718fc11f11b4227d
Islam Madkaur <islammadkaur@gmail.com>
2020-10-11 21:34:08 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -7,8 +7,12 @@ KATE_G = { name:  "Kate Gompert", occupation: "None" }
 BRUCE_G = { name:  "Bruce Green", occupation: "Fan of Mildred" }
 
 def assembled_aoh
-  occupation = {
+  people = {
     :DON_G => {name:  "Don Gately", occupation: "Live-in Staff"},
+    :JOELLE_VD => {name:  "Joelle van Dyne", occupation: "Radio Personality" },
+    :PAT_M  => {name:  "Pat Monteseian", occupation: "Staff" },
+    :KATE_G => {name:  "Kate Gompert", occupation: "None"},
+    :BRUCE_G => {name:  "Bruce Green", occupation: "Fan of Mildred"}
 
   }
 
